842f19700097c4299d12e4a2875e9a169b345982
brian.orwe <brian.orwe@gmail.com>
2020-05-05 20:58:26 +0300

    Update, removed GC prompt informing on method of starting javaagent

--- src/main/java/com/borwe/system/gc/App.java
+++ src/main/java/com/borwe/system/gc/App.java
@@ -7,20 +7,17 @@ import java.lang.instrument.Instrumentation;
 
 public class App {
 	
-	
 	public static void premain(String agentArgs,Instrumentation inst) {
-		System.out.println("Using Premain GC");
-//		getAllClassesAndSetupGC(agentArgs, inst);
         beginGCThread();
 	}
 
 	public static void agentmain(String agentArgs,Instrumentation inst) {
-		System.out.println("Using Agentmain GC");
         beginGCThread();
 	}
 	
     private static void beginGCThread(){
 		Thread thread=new Thread(()->{
+            //sleep for 5 minutes, before starting the GC stuff
 			while(true) {
 				try {
 					Thread.sleep(1002);
